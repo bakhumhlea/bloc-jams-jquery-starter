d451c7278703d9aec51412ea7b5b1633c5f3dd24
Bakhumhlea <Tatuu@bakhumhlea.com>
2017-10-29 23:24:57 -0700

    complete-checkpoint-12

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -4,7 +4,7 @@ const album = {
   releaseInfo: '2017',
   albumArtUrl: 'assets/images/album_covers/10.png',
   songs: [
-    { title: 'Parcels - Older', duration: '219.60', soundFileUrl: '"https://open.spotify.com/track/1kgws2l8gsvDhtsVyzWbu9' },
+    { title: 'Parcels - Overnight', duration: '219.00', soundFileUrl: 'assets/music/Overnight.m4a' },
     { title: 'Franz Ferdinand - Do You Want To', duration: '215.00', soundFileUrl: 'assets/music/Do You Want To.mp3' },
     { title: 'Foals - My Number', duration: '240.53', soundFileUrl: 'assets/music/My Number.mp3' },
     { title: 'Breakbot - Why (feat.Ruckazoid)', duration: '322.04', soundFileUrl: 'assets/music/Why (feat. Ruckazoid).mp3' },
